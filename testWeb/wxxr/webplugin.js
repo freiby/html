2a6b8114782fafe64cf574992611bf9e43d1dc3c
fudapeng <water541@sohu.com>
2015-03-16 01:40:46 +0800

    fff

--- testWeb/wxxr/webplugin.js
+++ testWeb/wxxr/webplugin.js
@@ -1,6 +1,12 @@
 /**
  * wxxr web plugin 
  */
+
+String.prototype.replaceAll = function(AFindText,ARepText) { 
+		raRegExp = new RegExp(AFindText,"g");
+	return this.replace(raRegExp,ARepText)
+}
+
 var WebPlugin = {
 	
 	init:function(){
@@ -12,18 +18,42 @@ var WebPlugin = {
 	initTab:function(region){
 		//初始化每个tab 都是未选中
 		$(".tab-nav-item-claz").each(function(){
+			var id = $(this).attr("id");
+			var vsItemid = id.replaceAll("\\.","-");
+			$(this).attr("id",vsItemid);
 			$(this).addClass("tabsUnSeleted");
 		});
+		
+		
+		$(".view-stack-item-claz").each(function(){
+			var id = $(this).attr("id");
+			var vsItemid = id.replaceAll("\\.","-");
+			$(this).attr("id",vsItemid);
+			$(this).hide();
+		});
+		
+		if(this.currentSelectedTab === ""){
+			this.currentSelectedTab = $(".tab-nav-item-claz:first");
+			this.currentSelectedTab.addClass("tabsSeleted");
+			this.currentSelectedTab.removeClass("tabsUnSeleted");
+			var tabid = $(".tab-nav-item-claz:first").attr("id");
+			var vsItemid = "#" + tabid;
+			$(".view-stack-wrapper-claz").children(vsItemid).show();
+		}
+		
 		if(this.currentSelectedTab === ""){
 			this.currentSelectedTab = $(".tab-nav-item-claz:first");
 			this.currentSelectedTab.addClass("tabsSeleted");
 			this.currentSelectedTab.removeClass("tabsUnSeleted");
 		}
+		
+		
 		this.selectedTab.webplugin = this;
 		$(".tab-nav-item-claz").click(this.selectedTab);
 		this.selectedTab.callAjax = this.callAjax;
 	},
 	selectedTab:function(){
+		if($(this) == webplugin.currentSelectedTab)return;
 		$(this).addClass("tabsSeleted");
 			$(this).removeClass("tabsUnSeleted");
 			var webplugin = arguments.callee.webplugin;
@@ -32,9 +62,21 @@ var WebPlugin = {
 				webplugin.currentSelectedTab.addClass("tabsUnSeleted");
 			}
 			webplugin.currentSelectedTab = $(this);
-			var options = {url:"Load.txt",success:function(){
-				renderTarget("a","b");;
-			},target:"aaa"};
+			
+			
+			$(".view-stack-item-claz").each(function(){
+				$(this).hide();
+			});
+			
+			var tabid = $(this).attr("id");
+			var vsItemid = "#" + tabid;
+			$(".view-stack-wrapper-claz").children(vsItemid).show();
+			var selectedView = tabid.replaceAll("-","\\.");
+			var options = 
+			{url:"myWorkbenchServlet\com.wxxr.mx.ui.showView.action",param:{selectedViewId:selectedView},success:function(data,status){
+				if(status === "success")
+				renderTarget(vsItemid,data);
+			}};
 			var callAjax = arguments.callee.callAjax;
 			callAjax(options);
 			
@@ -43,7 +85,8 @@ var WebPlugin = {
 		var url = options['url'];
 		var contextArg = options.context;
 		var successfn = options.success;
-		$.ajax({ url: url, context: contextArg, success: successfn});
+		var param = options.param
+		$.post(url,param,successfn);
 	},
 	renderTarget:function(target,content){
 		console.debug("render " + target + "content " + content);
