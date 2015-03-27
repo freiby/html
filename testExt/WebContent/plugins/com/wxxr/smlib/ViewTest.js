53267ece1d227059cbf459c3e38d9b947fd5fcae
fudapeng <fudapeng@7500.com.cn>
2015-03-27 18:39:30 +0800

    commit

---
+++ testExt/WebContent/plugins/com/wxxr/smlib/ViewTest.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+Ext.define('Smlib.com.wxxr.smlib.ViewTest', {
+	extends:'xxx',
+	age:1,
+	name:'fdp',
+	smlib_container:{},
+	selectionChange:function(selection){
+		alert(Ext.get('smlib_container').getHTML());
+	},
+	init:function(){
+		smlib_container = getfa.lgetj9
+	},
+	hello2:function(){
+		smlib_container.addClass();
+	},
+	getHTML:function(){
+		return "";
+	},
+	createPartControl:function(parent){
+		
+	}
+	
+});
+	
