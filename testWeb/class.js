541211b7606c9650a556df0558c1f95036cb375c
fudapeng <fudapeng@7500.com.cn>
2015-07-06 22:16:56 +0800

    commit

---
+++ testWeb/class.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+
+
+		function chain(o){
+			return EObject.chain(o);
+		};
+		var TemplateClass = function(){};
+
+		var EObject = {
+			chain: Object.create ||  function (object) {
+				TemplateClass.prototype = object;
+				var result = new TemplateClass();
+				TemplateClass.prototype = null;
+				return result;
+			}
+		};
+
+
+		var x_base = function x_base(){};
+		x_base.prototype = {name:"base"};
+
+		var	x_parent = function x_parent(){};
+		x_parent.prototype = chain(x_base.prototype);
+		x_parent.prototype.xname = "xparent";
+		
+		
+
+		var	x_children = function x_children(){};
+		x_children.prototype = chain(x_parent.prototype);
+		x_children.prototype.name = "x_children";
+		
+
+		var newIns = new x_children();
+		console.log(newIns.name);
+		console.log(newIns.xname);
