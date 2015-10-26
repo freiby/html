a5682a6421a593db17b92212e30a1b7ab39713ba
fudapeng <water541@sohu.com>
2015-10-26 23:26:55 +0800

    cjommit

--- mylabs/jquery/slide/js/slide.js
+++ mylabs/jquery/slide/js/slide.js
@@ -11,6 +11,56 @@
  			alert('ok');
  		}
  	};
+ 	this.options = $.extends{
+ 		play: false,
+	    animation_speed: 600,
+	    animation_easing: 'swing',
+	    animation: 'slide',
+	    inherit_width_from: window,
+	    inherit_height_from: window,
+	    pagination: true,
+	    hashchange: false,
+	    scrollable: true,
+	    elements: {
+	      preserve: '.preserve',
+	      nav: '.slides-navigation',
+	      container: '.slides-container',
+	      pagination: '.slides-pagination'
+	    },options
+ 	};
+ 	var el$ = $(el);
+ 	var slide_container$ =  el$.find(this.options.elements.container);
+ 	var control$ = $('<div>',{'class':'slide-control'})
+
+ 	var _current = 0;
+
+ 	Slide.prototype = {
+ 		size:function(){
+ 			return slide_container.children().lenght();
+ 		},
+ 		next:function(){
+ 			if(this.current >= this.size() - 1){
+ 				this.current = 0;
+ 			}else{
+ 				this.current += 1;
+ 			}
+ 		},
+ 		pre:function(){
+ 			if(this.current >= 0){
+ 				this.current = this.size() - 1;
+ 			}else{
+ 				this.current -= 1;
+ 			}
+ 		},
+ 		findWidth:function(){
+ 			return $(this.options.inherit_width_from).width();
+ 		},
+ 		findHeight:function(){
+ 			return $(this.options.inherit_width_from).height();
+ 		}
+ 	};
+
+ 	
 
  	$.fn.slide = function(options){
  		this.each(function(){
