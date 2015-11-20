754e091b6e7eef475696c635f991c0717771893b
fudapeng <icefirefdp@163.com>
2015-11-20 17:34:34 +0800

    OMIT

---
+++ mylabs/server/strusts/struts-test/tiles-app/src/main/java/com/wxxr/tiles/example/ProductAction.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.wxxr.tiles.example;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author fudapeng
+ *
+ */
+public class ProductAction extends ActionSupport {
+	public String tiger() { return "tiger"; }
+	public String lion() { return "lion"; }
+}
