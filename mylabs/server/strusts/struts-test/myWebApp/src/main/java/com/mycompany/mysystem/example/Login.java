7217fe64d0da94c95873832594f80a40c29b81dd
fudapeng <icefirefdp@163.com>
2015-12-15 18:53:21 +0800

    commit

--- mylabs/server/strusts/struts-test/myWebApp/src/main/java/com/mycompany/mysystem/example/Login.java
+++ mylabs/server/strusts/struts-test/myWebApp/src/main/java/com/mycompany/mysystem/example/Login.java
@@ -23,37 +23,39 @@ package com.mycompany.mysystem.example;
 
 public class Login extends ExampleSupport {
 
-    public String execute() throws Exception {
+	public String execute() throws Exception {
 
-        if (isInvalid(getUsername())) return INPUT;
+		if (isInvalid(getUsername()))
+			return INPUT;
 
-        if (isInvalid(getPassword())) return INPUT;
+		if (isInvalid(getPassword()))
+			return INPUT;
 
-        return SUCCESS;
-    }
+		return SUCCESS;
+	}
 
-    private boolean isInvalid(String value) {
-        return (value == null || value.length() == 0);
-    }
+	private boolean isInvalid(String value) {
+		return (value == null || value.length() == 0);
+	}
 
-    private String username;
+	private String username;
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    private String password;
+	private String password;
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
--- mylabs/server/strusts/struts-test/myWebApp/src/test/java/com/mycompany/mysystem/example/HelloWorldTest.java
+++ mylabs/server/strusts/struts-test/myWebApp/src/test/java/com/mycompany/mysystem/example/HelloWorldTest.java
@@ -26,12 +26,14 @@ import org.apache.struts2.StrutsTestCase;
 
 public class HelloWorldTest extends StrutsTestCase {
 
-    public void testHelloWorld() throws Exception {
-        HelloWorld hello_world = new HelloWorld();
-        String result = hello_world.execute();
-        assertTrue("Expected a success result!",
-                ActionSupport.SUCCESS.equals(result));
-        assertTrue("Expected the default message!",
-                hello_world.getText(HelloWorld.MESSAGE).equals(hello_world.getMessage()));
-    }
+	public void testHelloWorld() throws Exception {
+		HelloWorld hello_world = new HelloWorld();
+		String result = hello_world.execute();
+		assertTrue("Expected a success result!",
+				ActionSupport.SUCCESS.equals(result));
+		assertTrue(
+				"Expected the default message!",
+				hello_world.getText(HelloWorld.MESSAGE).equals(
+						hello_world.getMessage()));
+	}
 }
